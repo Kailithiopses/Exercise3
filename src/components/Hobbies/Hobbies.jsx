d665409a4f588b8afaa2a74daf6d6baae992af64
Kailithiopses <connereames@mail.weber.edu>
2026-05-29 20:16:53 -0600

    debugging

--- src/components/Hobbies/Hobbies.jsx
+++ src/components/Hobbies/Hobbies.jsx
@@ -1,22 +1,11 @@
 import HobbyItem from "./HobbyItem";
 import "./HobbiesStylesheets/Hobbies.css";
 import Card from "../UI/Card";
-import HobbiesFilter from "./HobbiesFilter";
 import { useState } from "react";
 
 function Hobbies(props) {
-  const [filteredTime, setFilteredTime] = useState("2023");
-
-  const filterChangeHandler = (selectedTime) => {
-    setFilteredTime(selectedTime);
-  };
-
   return (
     <Card className="hobbies">
-      <HobbiesFilter
-        selected={filteredTime}
-        onChangeFilter={filterChangeHandler}
-      />
       <HobbyItem
         title={props.items[0].title}
         timeSpent={props.items[0].timeSpent}
--- src/components/NewHobbies/HobbyForm.jsx
+++ src/components/NewHobbies/HobbyForm.jsx
@@ -1,31 +1,62 @@
 import "./NewHobbiesStylesheets/HobbyForm.css";
-import { useState } from "react";
+import { use, useState } from "react";
 
 const HobbyForm = (props) => {
+  //const [isValid, setIsValid] = useState(true);
+  const [titleIsValid, setTitleIsValid] = useState(true);
+  const [descIsValid, setDescIsValid] = useState(true);
+  const [amountIsValid, setAmountIsValid] = useState(true);
+
   const [enteredTitle, setEnteredTitle] = useState("");
   const titleChangeHandler = (event) => {
-    setEnteredTitle(event.target.value);
-  };
+    if (event.target.title.trim().length > 0) {
+      setTitleIsValid(true);
+    }
 
-  const [enteredAmount, setEnteredAmount] = useState("");
-  const amountChangeHandler = (event) => {
-    setEnteredAmount(event.target.value);
+    setEnteredTitle(event.target.title);
   };
 
   const [enteredDesc, setEnteredDesc] = useState("");
   const descChangeHandler = (event) => {
-    setEnteredDesc(event.target.value);
+    if (event.target.desc.trim().length > 0) {
+      setDescIsValid(true);
+    }
+
+    setEnteredDesc(event.target.desc);
+  };
+
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const amountChangeHandler = (event) => {
+    if (event.target.amount.trim().length > 0) {
+      setAmountIsValid(true);
+    }
+
+    setEnteredAmount(event.target.amount);
   };
 
   const submitHandler = (event) => {
-    event.preventDefault();
+    // Handle clicking submit button
+    event.preventDefault(); // Prevent page from refreshing
+    if (hobbyData.title.trim().length === 0) {
+      setTitleIsValid(false);
+      return;
+    }
+
+    if (hobbyData.desc.trim().length === 0) {
+      setDescIsValid(false);
+      return;
+    }
+
+    if (hobbyData.amount.trim().length === 0) {
+      setAmountIsValid(false);
+      return;
+    }
 
     const hobbyData = {
       title: enteredTitle,
       desc: enteredDesc,
       amount: enteredAmount,
     };
-
     props.onSaveHobbyData(hobbyData);
 
     setEnteredTitle("");
@@ -37,30 +68,48 @@ const HobbyForm = (props) => {
     <form onSubmit={submitHandler}>
       <div className="new-hobby__controls">
         <div className="new-hobby__control">
-          <label>Title</label>
+          <label style={{ color: !titleIsValid ? "red" : "black" }}>
+            Title
+          </label>
           <input
             type="text"
-            value={enteredTitle}
+            style={{
+              borderColor: !titleIsValid ? "red" : "black",
+              backgroundColor: !titleIsValid ? "pink" : "white",
+            }}
+            title={enteredTitle}
             onChange={titleChangeHandler}
           />
         </div>
         <div className="new-hobby__control">
-          <label>Description</label>
+          <label style={{ color: !descIsValid ? "red" : "black" }}>
+            Description
+          </label>
           <input
             type="text"
+            style={{
+              borderColor: !descIsValid ? "red" : "black",
+              backgroundColor: !descIsValid ? "pink" : "white",
+            }}
             min="0.01"
             step="0.01"
-            value={enteredDesc}
+            desc={enteredDesc}
             onChange={descChangeHandler}
           />
         </div>
         <div className="new-hobby__control">
-          <label>Amount</label>
+          <label style={{ color: !amountIsValid ? "red" : "black" }}>
+            Amount
+          </label>
           <input
             type="text"
+            style={{
+              borderColor: !amountIsValid ? "red" : "black",
+              backgroundColor: !amountIsValid ? "pink" : "white",
+            }}
             min="0.01"
             step="0.01"
-            value={enteredAmount}
+            amount={enteredAmount}
             onChange={amountChangeHandler}
           />
         </div>
